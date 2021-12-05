af64957dedcd75dba0682e79e0b118356c5bdb64
daibeal <57094326+daibeal@users.noreply.github.com>
2021-12-05 03:19:39 +0100

    some commit

--- source-code/main.py
+++ source-code/main.py
@@ -16,6 +16,7 @@ from getpass import getpass
 
 
 
+
 # Config
 
 chrome_options = webdriver.ChromeOptions()
@@ -35,6 +36,8 @@ __email__ = "contact@andresbenites.es"
 __status__ = "Production"
 __main__info = [__author__, __copyright__, __credits__, __license__, __version__, __maintainer__, __email__, __status__]
 print('\n'.join(__main__info))
+
+
 
 #load data
 is_previous__data__available = os.path.isfile('./login.data')
